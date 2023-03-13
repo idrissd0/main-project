23227670daf0cf2e8c1f86bec71e4aa370dfc271
Driss_D <gtbdriss@gmail.com>
2023-03-13 17:28:21 +0100

    done confirmation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router'; //1) import Routes and RoterModule
 
 import { AppComponent } from './app.component';
+import { UserLoginComponent } from './components/user-login/user-login.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+
+const appRoute: Routes = [
+  {path: 'home', component: HomePageComponent},
+  {path: 'login', component: UserLoginComponent}
+]
+//3) create a routes
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserLoginComponent,
+    HomePageComponent
   ],
   imports: [
-    BrowserModule
-  ],
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot(appRoute)
+  ],//2) add RouterModule to imports
   providers: [],
   bootstrap: [AppComponent]
 })
