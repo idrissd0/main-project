eb26b3fe1acd4e1ee1916c1ef7970b860b06c44a
Driss_D <gtbdriss@gmail.com>
2023-03-21 18:35:01 +0000

    created the calendar and few events

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,26 +8,30 @@ import { AppComponent } from './app.component';
 import { UserLoginComponent } from './components/user-login/user-login.component';
 import { HomePageComponent } from './components/home-page/home-page.component';
 import { EventsListComponent } from './components/events-list/events-list.component';
+//after Installing the Angular connector, the core package, and FullCalendarApp plugins
+import { FullCalendarModule } from '@fullcalendar/angular'; //connect FullCalendarModule
 
 const appRoute: Routes = [
   {path: 'home', component: HomePageComponent},
-  {path: 'login', component: UserLoginComponent}
-]
-//3) create a routes
+  {path: 'login', component: UserLoginComponent},
+  {path: 'events', component: EventsListComponent}
+]//3) create a routes
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserLoginComponent,
     HomePageComponent,
-    EventsListComponent
+    UserLoginComponent,
+    EventsListComponent,
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     FormsModule,
-    RouterModule.forRoot(appRoute)
-  ],//2) add RouterModule to imports
+    RouterModule.forRoot(appRoute),//2) add RouterModule to imports
+    FullCalendarModule,  // register FullCalendar with your app
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/components/events-list/events-list.component.ts
+++ src/app/components/events-list/events-list.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { CalendarOptions } from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
 
 @Component({
   selector: 'app-events-list',
@@ -7,4 +9,74 @@ import { Component } from '@angular/core';
 })
 export class EventsListComponent {
 
+  ladate: Date = new Date();
+
+  EVENTS = [
+    {
+      title: 'answer to god -> Pray',
+      date: '2023-03-21'
+    },
+    {
+      title: 'meet god',
+      date: new Date()
+    },
+    {
+      title: 'Event 1',
+      start: '2023-03-22T14:00:00',
+      end: '2023-03-22T16:00:00'
+    },
+    {
+      title: 'Event 2',
+      start: '2023-03-23T14:00:00',
+      end: '2023-03-24T16:00:00'
+    }
+  ];
+
+  constructor( ) {
+
+  }
+
+  calendarOptions: CalendarOptions  = {
+    plugins:[dayGridPlugin],
+    initialView: 'dayGridMonth',
+    weekends: true,
+    events: this.EVENTS
+  }
+
 }
+
+
+
+//consume json data events
+// import { HttpClient } from '@angular/common/http';
+// @Component({
+//   selector: 'app-events-list',
+//   templateUrl: './events-list.component.html',
+//   styleUrls: ['./events-list.component.css']
+// })
+// export class EventsListComponent {
+
+//   constructor(private http: HttpClient) { }
+
+//   events!: any[];
+
+//   ngOnInit() {
+//     this.http.get("eventsList.json").subscribe(data => {
+//       this.events = data.events;
+//     });
+//   }
+
+//   calendarOptions: CalendarOptions = {
+//     initialView: 'dayGridMonth',
+//     headerToolbar: {
+//       left: 'prev,next today',
+//       center: 'title',
+//       right: 'dayGridMonth,timeGridWeek,timeGridDay'
+//     },
+//     eventColor: '#378006',
+//     eventTextColor: '#fff',
+//     eventClick: (info) => {
+//       console.log(info.event.title);
+//     }
+//   };
+// }
